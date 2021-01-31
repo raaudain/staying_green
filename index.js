bc22abd667df1649cc02235313415367fcfa2cf9
Ramon Audain <raudain123@gmail.com>
2021-01-31 05:00:12 +0000

    bot doesn't work with VPN

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-// For NASA APOD
+// NASA APOD
   // testing NASA api
   // able to console.log past dates for NASA APOD
   // working on recursion to get the dates to change
+
+// Automated bot
+  // bot doesn't work with VPN
